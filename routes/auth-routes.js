734a2a4de7d4ef2d2bad5e3d7813a0585c5f2df6
daniloxxv <danilosoares@gmail.com>
2019-01-31 21:09:35 -0600

    regex validations for username, password and email

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -8,9 +8,60 @@ router
   .get('/signup', (req, res, next)=>{
     res.render('auth/signup');
   })
-  .post('/signup', (req, res, next)=>{     
-     console.log(req.body)
+  .post('/signup', (req, res, next)=>{
+    
+    //Regex validation for username
+    if (!req.body.username){
+      let usernameError = "Usernames cannot be blank"
+      res.render('auth/signup', {usernameError})
+      return
+    }
+    if (/[^a-z]/.test(req.body.username)){
+      let usernameError = "Only lowercase letters are alowed"
+      res.render('auth/signup', {usernameError})
+      return
+    }
 
+    //Regex validation for password
+    if (!req.body.password){
+      let passwordError = "Passwords cannot be blank"
+      res.render('auth/signup', {passwordError})
+      return
+    }
+    if (/[^a-zA-Z0-9]/.test(req.body.password)){
+      let passwordError = "Only letters and numbers are allowed."
+      res.render('auth/signup', {passwordError})
+      return
+    }
+    else if (!/(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+/.test(req.body.password)){
+      let passwordError = "Please include at least one number and one capital letter."
+      res.render('auth/signup', {passwordError})
+      return
+    }
+    else if (!/^[a-zA-Z0-9]{6,20}$/.test(req.body.password)){
+      let passwordError = "Your password should have between 6 and 20 characters."
+      res.render('auth/signup', {passwordError})
+      return
+    }
+
+    //Validation for email )
+    if (!req.body.email){
+      let emailError = "The e-mail field cannot be blank."
+      res.render('auth/signup', {emailError})
+      return
+    }
+    //Regex validation for email (thanks to https://emailregex.com/
+
+    if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(req.body.email)){
+      let emailError = "Please introduce a valid e-mail address"
+      res.render('auth/signup', {emailError})
+      return
+    }
+
+
+
+    //Passport authentication
+    
     User.register( new User({username: req.body.username,  email: req.body.email }),
     req.body.password,
     function(err, account){
