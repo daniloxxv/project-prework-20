a05247091a41f92d132fe3ad20c6b2b7463a94f3
daniloxxv <danilosoares@gmail.com>
2019-02-02 17:20:21 -0600

    fixed breadcrumbs

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,13 +7,11 @@ const parts = [{ "text": 'Home', "link": '/' }];
     let part = here[i];
     let text = part ? part[0].toUpperCase() + part.slice(1,part.length) : "";
     let link = '/' + here.slice( 0, i + 1 ).join('/');
-    parts.push({ "text": text, "link": link });
+    part ? parts.push({ "text": text, "link": link }): part;
   }
-
 let breadcrumbs = $('#breadcrumblist')
 for (let i = 0; i < parts.length-1; i++){
     breadcrumbs.append(`<li class="breadcrumb-item" aria-current="page"><a href="${parts[i].link}">${parts[i].text}</a></li>`)
   }
-
   breadcrumbs.append(`<li class="breadcrumb-item active" aria-current="page">${parts.pop().text}</li>`)
 })
