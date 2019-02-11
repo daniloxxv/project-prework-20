250fb1f2ecd413768a18dd70a294fcf615c68b07
daniloxxv <danilosoares@gmail.com>
2019-02-10 23:27:03 -0600

    Added Quiz 2. TODO: debug validation

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -14,8 +14,22 @@ router.post("/quiz1", (req, res) => {
     {$set: {"quizAnswers.0": answer, "completedLessons.0":1}}
   )
   .then(()=>{
-    let successMessage = "<span class = 'alert-success'>You have completed the lesson</span>"
     res.redirect("/learning")
+  })
+  
+
+ });
+
+ router.post("/quiz2", (req, res) => {
+  let answer = req.body.quiz2;
+  let username = req.user.username;
+  //TODO: SAVE IN DATABASE
+  User.findOneAndUpdate(
+    {username: username},
+    {$set: {"quizAnswers.1": answer, "completedLessons.1":1}}
+  )
+  .then(()=>{
+    res.redirect("/lessons/2")
   })
   
 
