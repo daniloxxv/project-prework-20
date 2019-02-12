da7b9532a2edabebf5bad45807a5df0e42889a7b
JZunigaV <j_zuniga4@hotmail.com>
2019-02-12 00:33:18 -0600

    Added input validations

--- public/javascripts/fileInputBehaviour.js
+++ public/javascripts/fileInputBehaviour.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
-  $("#input-b2").fileinput({ showUpload: false });
-
+  $("#input-b2").fileinput({
+    showUpload: false,
+    allowedFileExtensions: ["jpg", "png", "gif"]
+  });
 
   $("#btn-submit").click(function() {
-    $.blockUI({
-      message: '<img src="/images/1.gif" />',
-      css: {
-        border: "none",
-        backgroundColor: "transparent"
-      }
-    });
   
+    var fileInput = $(".file-caption-name");
+    var title = fileInput.attr("title");
+
+    if (title === "Validation Error") {
+      return;
+    } else {
+      $.blockUI({
+        message: '<img src="/images/1.gif" />',
+        css: {
+          border: "none",
+          backgroundColor: "transparent"
+        }
+      });
+    }
   });
 });
